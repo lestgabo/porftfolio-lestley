ab1e0e275b16cce69b55baf098d430ea4599ddb1
lestgabo <lestgabo@gmail.com>
2020-08-21 04:11:06 -0700

    Fix error - unused vars

--- src/@tacogator/gatsby-theme-blog-material-clarisse/components/blog.js
+++ src/@tacogator/gatsby-theme-blog-material-clarisse/components/blog.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Typography, Container } from '@material-ui/core';
 
 import Listing from '@tacogator/gatsby-theme-blog-material-clarisse/src/components/listing';
-import useMinimalBlogConfig from '@tacogator/gatsby-theme-blog-material-clarisse/src/components/use-minimal-blog-config';
 import SEO from '@tacogator/gatsby-theme-blog-material-clarisse/src/components/seo';
 import { SectionDivider, useUtilStyles } from '@tacogator/gatsby-theme-blog-material-clarisse/src/components/utils';
 
